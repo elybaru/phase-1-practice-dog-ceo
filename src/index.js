569c502c09342b052c62f55a033ec8dcd73a71bf
Ely Karr <elykarr@gmail.com>
2021-10-15 20:41:17 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log('%c HI', 'color: firebrick')
+
+// Challenge 1
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => randomDogFunction(data))
+
+function randomDogFunction(data) {
+    data.message.forEach(urlLink => {
+        let img = document.createElement("img")
+        img.src = urlLink
+        img.style.maxWidth = "300px"
+        let dogImageContainer = document.querySelector("#dog-image-container")
+        dogImageContainer.append(img)
+    })
+}
+
+// Challenge 2
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(data => dogBreedFunction(data))
+
+function dogBreedFunction(data) {
+}
+
+
+
+
