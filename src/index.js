5102dcdf6f4d3ee9abe6540d0484029c98b1c5a0
Ely Karr <elykarr@gmail.com>
2021-10-17 13:58:26 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -36,24 +36,107 @@ function renderAllBreeds(breeds) {
 }
 
 function renderEachBreed(breed) {
+    console.log(breed)
     let li = document.createElement("li")
     li.innerText = breed
+    li.addEventListener("click", changeColor)
     dogList.appendChild(li)
+    let dropdown = document.getElementById("breed-dropdown")
+
+    dropdown.addEventListener("change", filterBreedByLetter)
+    // map the data from all breeds to a shorter list by value reasign breed variable
+    // this event listener will push to a callback the letter
+    // then in this callback, it will filter all the breeds saved into allbreeds by the value of letter
+    // then it will push the filtered result back into renderEachBreed
+    // then it will be printed to the DOM with lis. 
 }
+
+// We are fetching breed URL
+// We are turning into response, jsonifying it
+// then taking data from json then filtering by object keys of the data keys 
+// renderAllbreeds is called, first assigning the filtered keys to a globally scoped variable
+// then calling a function and inputing the filtered data into the parameter, to renderEachBreed
+// 
+function filterBreedByLetter() {
+    // console.log(allBreeds)
+    let letter = document.getElementById("breed-dropdown").value
+    const filteredBreeds = allBreeds.filter(function (item) {
+        return item[0] === letter;
+    })
+    console.log(filteredBreeds)
+    let noBreedsFound;
+    dogList.innerHTML = ''
+    console.log(filteredBreeds.length)
+    if (filteredBreeds.length > 0) {
+        filteredBreeds.forEach(renderEachBreed)
+    } else {
+        dogList.innerHTML = `<p>There are no doggos that start with the letter ${letter}</p>`
+    }
+    // renderEachBreed(filteredBreeds);
+
+
+}
+
 
 // Challenge 3
 
-// let a = document.querySelector("a")
-// console.log(a)
+function changeColor(event) {
+    let colorText = event.target.style.color
+    if (colorText === "black") {
+        event.target.style.color = "red"
+    } else {
+        event.target.style.color = "black"
+    }
+}
 
-let li = document.querySelectorAll("li")
-// console.log(li)
+// Challenge 4
 
-document.querySelectorAll('li').forEach(item => {
-    item.addEventListener('click', event => {
-        item.style.color = "red"
-    })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// What's the deal with semicolons? Best practice?
+
+// Challenge 3 option with boolean
+// function changeColor(event) {
+//     let colorText;
+//     if (!colorText) {
+//         event.target.style.color = "red"
+//         colorText = false
+//     } else {
+//         event.target.style.color = "black"
+//         colorText = true
+//     }
+// }
+
+// Why didn't this work in Challenge 3?
+// Array.from(li).forEach(item => {
+//     console.log(item)
+//     // item.addEventListener('click', event => {
+//     //     item.style.color = "red"
+//     // })
+// })
 
 
 
@@ -62,11 +145,3 @@ document.querySelectorAll('li').forEach(item => {
 //         e.target.style.color = "red"
 //     })
 // })
-
-// a.addEventListener("click", () => {
-//     a.style.color = "red"
-// })
-
-// function breedClick(event) {
-//     event.style.color = "red"
-// }
